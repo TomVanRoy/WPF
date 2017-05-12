a989636a3a6d4969153464fe1d331819a7f53699
Tom Van Roy <tom.van.roy@hotmail.com>
2017-05-12 15:53:40 +0200

    Einde dag 2017-05-12

--- Bars/MainWindow.xaml.cs
+++ Bars/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,53 +23,75 @@ namespace Bars
     /// </summary>
     public partial class BarWindow : Window
     {
+        private double A4breedte = 21 / 2.54 * 96;
+        private double A4hoogte = 29.7 / 2.54 * 96;
+        private double vertPositie;
+
         public BarWindow()
         {
             InitializeComponent();
-
+            /*
             CommandBinding mijnCtrlB = new CommandBinding(mijnRouteCtrlB, ctrlBExecuted);
             this.CommandBindings.Add(mijnCtrlB);
             KeyGesture toetsCtrlB = new KeyGesture(Key.B, ModifierKeys.Control);
             KeyBinding mijnKeyCtrlB = new KeyBinding(mijnRouteCtrlB, toetsCtrlB);
             this.InputBindings.Add(mijnKeyCtrlB);
+
             CommandBinding mijnCtrlI = new CommandBinding(mijnRouteCtrlI, ctrlIExecuted);
             this.CommandBindings.Add(mijnCtrlI);
             KeyGesture toetsCtrlI = new KeyGesture(Key.I, ModifierKeys.Control);
             KeyBinding mijnKeyCtrlI = new KeyBinding(mijnRouteCtrlI, toetsCtrlI);
             this.InputBindings.Add(mijnKeyCtrlI);
+            */
         }
-
-        private void Vet_Aan_Uit()
+        private void Vet_Aan_Uit(Boolean wissel = false)
         {
-            if (TextBoxVoorbeeld.FontWeight == FontWeights.Normal)
+            if ((wissel == true && TextBoxVoorbeeld.FontWeight == FontWeights.Bold) || (wissel == false && TextBoxVoorbeeld.FontWeight == FontWeights.Normal))
             {
                 TextBoxVoorbeeld.FontWeight = FontWeights.Bold;
                 MenuVet.IsChecked = true;
+                StatusVet.FontWeight = FontWeights.Bold;
+                ButtonVet.IsChecked = true;
             }
             else
             {
                 TextBoxVoorbeeld.FontWeight = FontWeights.Normal;
                 MenuVet.IsChecked = false;
+                StatusVet.FontWeight = FontWeights.Normal;
+                ButtonVet.IsChecked = false;
             }
         }
         private void MenuVet_Click(object sender, RoutedEventArgs e)
         {
             Vet_Aan_Uit();
         }
-        private void Schuin_Aan_Uit()
+        private void ctrlBExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            if (TextBoxVoorbeeld.FontStyle == FontStyles.Normal)
+            Vet_Aan_Uit();
+        }
+        private void Schuin_Aan_Uit(Boolean wissel = false)
+        {
+            if ((wissel == true && TextBoxVoorbeeld.FontStyle == FontStyles.Italic) || (wissel == false && TextBoxVoorbeeld.FontStyle == FontStyles.Normal))
             {
                 TextBoxVoorbeeld.FontStyle = FontStyles.Italic;
                 MenuSchuin.IsChecked = true;
+                StatusSchuin.FontStyle = FontStyles.Italic;
+                ButtonSchuin.IsChecked = true;
             }
             else
             {
                 TextBoxVoorbeeld.FontStyle = FontStyles.Normal;
                 MenuSchuin.IsChecked = false;
+                StatusSchuin.FontStyle = FontStyles.Normal;
+                ButtonSchuin.IsChecked = false;
             }
         }
+
         private void MenuSchuin_Click(object sender, RoutedEventArgs e)
+        {
+            Schuin_Aan_Uit();
+        }
+        private void ctrlIExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             Schuin_Aan_Uit();
         }
@@ -78,8 +103,158 @@ namespace Bars
                 huidig.IsChecked = false;
             }
             hetLettertype.IsChecked = true;
-            TextBoxVoorbeeld.FontFamily =
-            new FontFamily(hetLettertype.Header.ToString());
+            LettertypeComboBox.SelectedItem = new FontFamily(hetLettertype.Header.ToString());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LettertypeComboBox.Items.SortDescriptions.Add(new SortDescription("Source", ListSortDirection.Ascending));
+            LettertypeComboBox.SelectedItem = new FontFamily(TextBoxVoorbeeld.FontFamily.ToString());
+        }
+
+        private void LettertypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (MenuItem huidig in Fontjes.Items)
+            {
+                if (LettertypeComboBox.SelectedItem.ToString() == huidig.Header.ToString())
+                    huidig.IsChecked = true;
+                else
+                    huidig.IsChecked = false;
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.FileName = "Document";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "Text documents |*.txt";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamWriter bestand = new StreamWriter(dlg.FileName))
+                    {
+                        bestand.WriteLine(LettertypeComboBox.SelectedValue);
+                        bestand.WriteLine(TextBoxVoorbeeld.FontWeight.ToString());
+                        bestand.WriteLine(TextBoxVoorbeeld.FontStyle.ToString());
+                        bestand.WriteLine(TextBoxVoorbeeld.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Opslaan mislukt: " + ex.Message);
+                throw;
+            }
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.FileName = "Document";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "Text documents |*.txt";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamReader bestand = new StreamReader(dlg.FileName))
+                    {
+                        LettertypeComboBox.SelectedValue = new FontFamily(bestand.ReadLine());
+                        TypeConverter convertBold = TypeDescriptor.GetConverter(typeof(FontWeight));
+                        TextBoxVoorbeeld.FontWeight = (FontWeight)convertBold.ConvertFromString(bestand.ReadLine());
+                        Vet_Aan_Uit(true);
+
+                        TypeConverter convertStyle = TypeDescriptor.GetConverter(typeof(FontStyle));
+                        TextBoxVoorbeeld.FontStyle = (FontStyle)convertStyle.ConvertFromString(bestand.ReadLine());
+                        Schuin_Aan_Uit(true);
+
+                        TextBoxVoorbeeld.Text = bestand.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Openen mislukt: " + ex.Message);
+            }
+        }
+
+        private void PrintExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                PrintDialog afdrukken = new PrintDialog();
+                if (afdrukken.ShowDialog() == true)
+                {
+                    afdrukken.PrintDocument(StelAfdrukSamen().DocumentPaginator, "tekstbox");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Printen gefaald: " + ex.Message);
+            }
+        }
+
+        private FixedDocument StelAfdrukSamen()
+        {
+            FixedDocument document = new FixedDocument();
+            document.DocumentPaginator.PageSize = new System.Windows.Size(A4breedte, A4hoogte);
+
+            PageContent inhoud = new PageContent();
+            document.Pages.Add(inhoud);
+
+            FixedPage page = new FixedPage();
+            inhoud.Child = page;
+
+            page.Width = A4breedte;
+            page.Height = A4hoogte;
+            vertPositie = 96;
+
+            page.Children.Add(Regel("Gebruikt lettertype: " + TextBoxVoorbeeld.FontFamily.ToString()));
+            page.Children.Add(Regel("Gewicht lettertype: " + TextBoxVoorbeeld.FontWeight.ToString()));
+            page.Children.Add(Regel("Gebruikte stijl: " + TextBoxVoorbeeld.FontStyle.ToString()));
+            page.Children.Add(Regel(""));
+            page.Children.Add(Regel("Inhoud tekstbox: "));
+            for (int i = 0; i < TextBoxVoorbeeld.LineCount; i++)
+            {
+                page.Children.Add(Regel(TextBoxVoorbeeld.GetLineText(i)));
+            }
+            return document;
+        }
+
+        private TextBlock Regel(string tekst)
+        {
+            TextBlock deRegel = new TextBlock();
+            deRegel.Text = tekst;
+            deRegel.FontSize = TextBoxVoorbeeld.FontSize;
+            deRegel.FontFamily = TextBoxVoorbeeld.FontFamily;
+            deRegel.FontWeight = TextBoxVoorbeeld.FontWeight;
+            deRegel.FontStyle = TextBoxVoorbeeld.FontStyle;
+            deRegel.Margin = new Thickness(96, vertPositie, 96, 96);
+            vertPositie += 30;
+            return deRegel;
+        }
+
+        private void PrintPreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Afdrukvoorbeeld preview = new Afdrukvoorbeeld();
+            preview.Owner = this;
+            preview.AfdrukDocument = StelAfdrukSamen();
+            preview.ShowDialog();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("Programma afsluiten?", "Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
         }
     }
     public partial class BarWindow : Window
---
+++ ParkingBon/ParkingBonWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParkingBon
+{
+    /// <summary>
+    /// Interaction logic for ParkingBonWindow.xaml
+    /// </summary>
+    public partial class ParkingBonWindow : Window
+    {
+        public ParkingBonWindow()
+        {
+            InitializeComponent();
+            Nieuw();
+        }
+
+        private void Nieuw()
+        {
+            DatumBon.SelectedDate = DateTime.Now;
+            AankomstLabelTijd.Content = DateTime.Now.ToLongTimeString();
+            TeBetalenLabel.Content = "0 €";
+            VertrekLabelTijd.Content = AankomstLabelTijd.Content;
+            StatusBarLabel.Content = "nieuwe bon";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Programma afsluiten ?", "Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void minder_Click(object sender, RoutedEventArgs e)
+        {
+            int bedrag = Convert.ToInt32(TeBetalenLabel.Content.ToString().Replace(" €", ""));
+            if (bedrag > 0)
+            {
+                bedrag -= 1;
+            }
+            TeBetalenLabel.Content = bedrag.ToString() + " €";
+            VertrekLabelTijd.Content = Convert.ToDateTime(AankomstLabelTijd.Content).AddHours(0.5 * bedrag).ToLongTimeString();
+        }
+
+        private void meer_Click(object sender, RoutedEventArgs e)
+        {
+            int bedrag = Convert.ToInt32(TeBetalenLabel.Content.ToString().Replace(" €", ""));
+            DateTime vertrekuur = Convert.ToDateTime(AankomstLabelTijd.Content).AddHours(0.5 * bedrag);
+            if (vertrekuur.Hour < 22)
+            {
+                bedrag += 1;
+            }
+            TeBetalenLabel.Content = bedrag.ToString() + " €";
+            VertrekLabelTijd.Content = Convert.ToDateTime(AankomstLabelTijd.Content).AddHours(0.5 * bedrag).ToLongTimeString();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.FileName = "Bon";
+                dlg.DefaultExt = ".bon";
+                dlg.Filter = "Text documents |*.bon";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamReader bestand = new StreamReader(dlg.FileName))
+                    {
+                        var line = bestand.ReadLine();
+                        var datetimeString = line.Split('/');
+                        DatumBon.SelectedDate = new DateTime(int.Parse(datetimeString[2]), int.Parse(datetimeString[1]), int.Parse(datetimeString[0]));
+                        AankomstLabelTijd.Content = bestand.ReadLine();
+                        TeBetalenLabel.Content = bestand.ReadLine();
+                        VertrekLabelTijd.Content = bestand.ReadLine();
+                    }
+                    StatusBarLabel.Content = System.IO.Path.GetFullPath(dlg.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("openen mislukt : " + ex.Message);
+            }
+        }
+        private void NewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Nieuw();
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.FileName = DateTime.Today.ToString("dd-MM-yyyy") + "om" + DateTime.Now.ToString("HH-mm-ss");
+                dlg.DefaultExt = ".bon";
+                dlg.Filter = "Text documents |*.bon";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamWriter bestand = new StreamWriter(dlg.FileName))
+                    {
+                        bestand.WriteLine(DatumBon.SelectedDate.Value.ToShortDateString());
+                        bestand.WriteLine(AankomstLabelTijd.Content.ToString());
+                        bestand.WriteLine(TeBetalenLabel.Content.ToString());
+                        bestand.WriteLine(VertrekLabelTijd.Content.ToString());
+
+                    }
+                    StatusBarLabel.Content = System.IO.Path.GetFullPath(dlg.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("opslaan mislukt: " + ex.Message);
+            }
+        }
+
+        private void PrintPreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            PrintPreview preview = new PrintPreview();
+            preview.Owner = this;
+            preview.AfdrukDocument = StelAfdrukSamen();
+            preview.ShowDialog();
+        }
+
+
+        private FixedDocument StelAfdrukSamen()
+        {
+            FixedDocument document = new FixedDocument();
+            document.DocumentPaginator.PageSize = new System.Windows.Size(A4breedte, A4hoogte);
+            PageContent inhoud = new PageContent();
+            document.Pages.Add(inhoud);
+            FixedPage page = new FixedPage();
+            inhoud.Child = page;
+            page.Width = 640;
+            page.Height = 320;
+            return document;
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
